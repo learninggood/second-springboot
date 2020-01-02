f5f27be42803cf0bc2d4dee9452a0046326e066e
984761985@qq.com <786879138@qq.com>
2020-01-02 15:38:07 +0800

    ldt 01/02

---
+++ src/main/java/com/example/firstspringboot/FirstSpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.example.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringbootApplication.class, args);
+    }
+
+    @RequestMapping
+    public String hello() {
+        return "hello firstboot project";
+    }
+}
